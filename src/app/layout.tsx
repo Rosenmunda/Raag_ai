6e1fb93663fd5f8e2a5a7c8dbd382e67b3e41551
Anurag Sen <118921364+Rosenmunda@users.noreply.github.com>
2025-04-24 14:46:11 +0530

    maa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "CodeFlex AI - Get Jacked",
+  title: "CodeFlex AI - By Raag",
+  authors: [{ name: "Raag" }],
   description: "A modern fitness AI platform to get jacked for free.",
 };
 
